675e28cf086578f2a8e19c7ab218e014e846d30f
mrbtx <mr.teachman@gmail.com>
2023-01-20 03:02:13 -0800

    Completed trigger read.

--- sr_scanner.py
+++ sr_scanner.py
@@ -2,14 +2,8 @@
 import socket
 from time import sleep
 
-SCANNER_HOST =  "198.168.1.54"
+SCANNER_HOST =  "192.168.1.54"
 SCANNER_PORT = 9004            # socket server port number
-
-# Scanner ASCII Commands ------------------------------------------------------
-
-MESSAGE = b'LON[CR]',
-
-# -----------------------------------------------------------------------------
 
 
 """
@@ -23,29 +17,26 @@ class SRScanner:
         self.log = logger
         self.connect()
 
-    def tx(self, message):
-        self.conn.send(message)  # send message
-        sleep(.1)
-
-    def rx(self):
-        data = self.conn.recv(1024).decode()  # receive response
-        print('Received from scanner: ' + data)
-        # self.log.info('Received from scanner: ' + data)
-        return data
-
     # Initiate a connection to the SR1000
     def connect(self):
         self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Socket Connection
+        self.conn.settimeout(10)
         try:
             self.conn.connect((SCANNER_HOST, SCANNER_PORT))  # connect to the server
             sleep(.1)
-            data = self.rx()  # TODO: waht does this response contain?
+            # print(self.rx())
             self.connected = True
             print(f"Connected to {SCANNER_HOST}:{SCANNER_PORT}")
-            # self.log.info(f"Connected to {SCANNER_HOST}:{SCANNER_PORT}")
         except socket.error as er:
             print(f"Connection Failed: {er}")
-            # self.log.error(f"Connection to {SCANNER_HOST}:{SCANNER_PORT} Failed")
+
+    def tx(self, message):
+        self.conn.sendall(message)  # send message
+
+    def rx(self):
+        data = self.conn.recv(1024).decode()  # receive response
+        # print('Received from scanner: ' + data)
+        return data
         
     # Send/Receive a Message
     def req(self, message):
@@ -66,8 +57,36 @@ class SRScanner:
         pass
 
 
+# Scanner ASCII Commands ------------------------------------------------------
+
+# Read Serial Number
+TRIGGER_INPUT_ON = b'LON\r'
+TRIGGER_INPUT_OFF = b'LOFF\r'
+
+REQUEST_SCANNER_VERSION = b'KEYENCE\r'
+
+REQUEST_TEST = b'TEST1\r'
+
+# Set the timing mode to "One-shot trigger" (one-shot signal trigger)
+SET_ONESHOT_TRIGGER = b'WP,101,1\r'
+# Check if one shot trigger is set
+IS_ONESHOT_TRIGGER = b'RP,101\r'
+
+SET_TIME_APPENDING = b'WP,300,1\r'
+IS_TIME_APPENDING = b'RP,300,1\r'
+
+# -----------------------------------------------------------------------------
+
 if __name__ == '__main__':
 
     s = SRScanner()
 
-    print(s.req(MESSAGE))
+    try:
+
+        while True:
+            # Fetch serial number
+            print(s.req(TRIGGER_INPUT_ON))
+            sleep(1)
+
+    except KeyboardInterrupt:
+        print("Done")
